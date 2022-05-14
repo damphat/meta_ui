63496c98f0e86f6a5cce9298651e874c304adf0f
Phat <damphat@gmail.com>
2022-05-14 17:54:42 +0700

    TODO: add UpdateEntry, an entry whose value is recalculated frame by frame

---
+++ Runtime/Scripts/Entries/UpdateEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace MetaUI.Generic
+{
+    public class UpdateEntry<T> : Entry<T>, IUpdate
+    {
+        private T latest;
+        private Func<T> update;
+        private UnityEvent<T> uevent;
+        public UpdateEntry(string name, Func<T> update) : base(name)
+        {
+            latest = update();
+            uevent = new UnityEvent<T>();
+        }
+
+        public override T Get()
+        {
+            return latest;
+        }
+
+        public override void Set(T value)
+        {
+            latest = value;
+        }
+
+        public override void Add(UnityAction<T> action)
+        {
+            uevent.AddListener(action);
+        }
+
+        public override void Remove(UnityAction<T> action)
+        {
+            uevent.RemoveListener(action);
+        }
+
+        public void Update()
+        {
+            var next = update();
+            if (!EqualityComparer<T>.Default.Equals(next, latest))
+            {
+                latest = next;
+                uevent.Invoke(latest);
+            }
+        }
+    }
+}
